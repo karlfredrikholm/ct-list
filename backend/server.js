2ca1410b30f3eaa80cc6eb5a7d56e56b184f363e
Fredrik Holm <karlfredrikholm@gmail.com>
2022-12-28 12:18:33 +0100

    works but case sensitive

--- backend/server.js
+++ backend/server.js
@@ -43,12 +43,12 @@ app.get('/', (req, res) => {
 // GET  all cocktails
 app.get('/cocktails', async (req, res) => {
   const { name } = req.query;
-  // const searchQuery = name ? name : /.*/;
+  const nameQuery = name ? name : /.*/;
   let cocktailList = {};
 
   try {
     if (name) {
-      cocktailList = await Cocktail.find({ cocktailName: {$regex: name} }).sort({ cocktailName: 1});
+      cocktailList = await Cocktail.find({ cocktailName: {$regex: nameQuery} }).sort({ cocktailName: 1});
     } else {
       cocktailList = await Cocktail.find().sort({ cocktailName: 1});
     }
